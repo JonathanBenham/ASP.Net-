947aa192e2c8549d4c1bc9db82593b79c4b3cecd
JonathanBenham <59968422+JonathanBenham@users.noreply.github.com>
2020-11-08 19:00:24 +0200

    Add files via upload

---
+++ Restaurant List website/OdeToFood.core/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Core
+{
+
+    public class Restaurant
+    {
+
+       
+        public int Id { get; set; }
+
+        [Required, StringLength(25)]
+        public string Name { get; set; }
+
+        [Required, StringLength(255)]
+        public string Location { get; set; }
+
+        public CuisineType Cuisine  { get; set; }
+
+    }
+}
